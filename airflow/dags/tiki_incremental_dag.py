f0eeac6f744f817daeba972f2d147dfcd3454af3
pquyen-nek <phiquyentran28@gmail.com>
2026-05-29 13:31:59 +0000

    met & bun ngu

--- airflow/dags/tiki_incremental_dag.py
+++ airflow/dags/tiki_incremental_dag.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import os
 import sys
 
+# qa bun ngu rôi
 # Tự động lấy path của project
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.append(BASE_DIR)
